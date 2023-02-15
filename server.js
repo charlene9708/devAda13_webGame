4c3928e69d2d6f68d07e0d2cd053a8fa39ffd50f
charlene9708 <yuqianzhang97@gmail.com>
2023-02-15 17:46:42 -0600

    Added navbar and homepage style

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require("express")
 const app = express();
 app.set("view engine", "ejs")
 
-app.get("/", (req, res) => {
+//handle HTTP GET requests 
+app.get("/", (req, res) => { 
     res.render("homepage", {text: "girls!" })
+  });
+
+  app.get("/nav", (req, res) => { 
+    res.render("navbar")
   });
 app.listen(3000)
